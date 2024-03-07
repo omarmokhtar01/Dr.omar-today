4fd7d0329af4907b4cb0652258ff97fa376c0169
omarmokhtar <you@example.com>
2024-03-07 22:21:06 +0200

    four commit16

--- src/components/Pictures/Pictures.js
+++ src/components/Pictures/Pictures.js
@@ -18,17 +18,15 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { getAllPicuture } from '../../features/allPictres/allPicturesSlice';
 import { Link } from 'react-router-dom';
-import { getAllImgCategory } from '../../features/imgCategory/imgCategorySlice';
+import { getAllImgCategory, getOneImgCategory } from '../../features/imgCategory/imgCategorySlice';
 import { IoHeartCircleSharp } from 'react-icons/io5';
 import { MdDownloadForOffline, MdOutlineFavoriteBorder } from "react-icons/md";
 import { FaShare } from "react-icons/fa6";
 import { FaShareFromSquare } from "react-icons/fa6";
 import { saveAs } from 'file-saver';
 
-const images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8,pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8,pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8]; // Assuming you have imported these images
-const heartImg = <img src={heart1} style={{ position: 'absolute', zIndex: '2', margin: '15px' }} alt="heart" />; // Assuming you have imported heart1
-
 const Pictures = () => {
+  const [id,setId]=useState(null)
 
 //to make modal
 const [show, setShow] = useState(false);
@@ -51,12 +49,22 @@ const downloadImage=()=>{
   const isLoadingAllImgCategory = useSelector(state => state.imgCategory.isLoading);
   const errorAllImgCategory = useSelector(state => state.imgCategory.error);
   
+
+
+
+  const getOneData = useSelector((state) => state.imgCategory.OneImgsData);
+  const isLoadingOneImgCategory = useSelector(
+    (state) => state.imgCategory.isLoading
+  );
+
   useEffect(() => {
       dispatch(getAllPicuture());
       dispatch(getAllImgCategory());
   }, [dispatch]);
   
-  console.log('data is: ', getAllImgData);
+  useEffect(() => {
+    dispatch(getOneImgCategory(id));
+  }, [dispatch, id]);
 
  
   
@@ -75,7 +83,7 @@ const downloadImage=()=>{
 
     <Container className='d-flex justify-content-center align-items-center'>
   <Row className="m-3 d-flex" style={{ justifyContent: 'space-between' }}>
-    <Col xs="6" md="4" lg="2" style={{ textAlign: 'center', marginBottom: '10px' }}>
+    <Col xs="6" md="4" lg="2" style={{ textAlign: 'center', marginBottom: '10px' }} onClick={()=>setId(null)}>
       <Link style={{ color: 'rgba(5, 20, 39, 1)', fontSize: '15px', marginTop: '5px', textDecoration: 'none' }} to={'/pictures'}>
         <div style={{ border: 'none', borderRadius: '23px', width: '124px', height: '33.74px', background: 'linear-gradient(331.41deg, #D19B6F 6.78%, #F6E5C3 204.87%)', boxShadow: '0px 3.6861166954040527px 3.6861166954040527px 0px rgba(209, 155, 111, 0.22)' }}>
           <p style={{ color: '#FFFFFF', fontWeight: 'bold' }}>الكل</p>
@@ -83,7 +91,7 @@ const downloadImage=()=>{
       </Link>
     </Col>
 
-    {!isLoadingAllImgCategory ? (
+    {
       getAllImgData ? (
         <>
           {getAllImgData.map((img, index) => (
@@ -97,7 +105,7 @@ const downloadImage=()=>{
           ))}
         </>
       ) : null
-    ) : null}
+  }
 
   </Row>
 
@@ -110,7 +118,9 @@ const downloadImage=()=>{
 
      <Container>
   <Row className="row row-cols-2 row-cols-lg-4 g-2 g-lg-3" style={{ margin: '35px' }}>
-    {!isLoadingAllPictures ? (
+    {
+    id == null ? (
+    !isLoadingAllPictures ? (
       getAllPicturesData.length > 0 ? (
         getAllPicturesData.map((image, index) => (
           <Col key={index} xl={3} lg={4} md={6} sm={12}>
@@ -129,8 +139,32 @@ const downloadImage=()=>{
             />
           </Col>
         ))
-      ) : <div style={{height:'140px'}}></div>
-    ) :<div style={{height:'140px'}}> <Spinner animation="border" variant="primary" /></div>
+      ) : <div style={{height:'240px'}}></div>
+    ) :<div style={{height:'240px'}}> <Spinner animation="border" variant="primary" /></div>
+    ) :
+    !isLoadingOneImgCategory ? (
+      getOneData.length > 0 ? (
+        getOneData.map((image, index) => (
+          <Col key={index} xl={3} lg={4} md={6} sm={12}>
+            {/* Placeholder for heartImg */}
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '60px', gap: '20px', position: 'absolute' }}>
+              <Link to={"/favpictures"}>  <IoHeartCircleSharp style={{ color: '#878787bd', fontSize: '30px', marginRight: '35px', marginTop: '10px' }} /></Link>
+            </div>
+            <img
+              src={image.image}
+              width={250}
+              height={350}
+              alt={`pic${index + 1}`}
+              style={{ marginBottom: '35px', borderRadius: '15px' }}
+              onClick={() => handleShow(image.image)}
+              id='img-responsive-pic'
+            />
+          </Col>
+        ))
+      ) : <div style={{height:'240px'}}> </div>
+    ):<div style={{height:'240px'}}> <Spinner animation="border" variant="primary" /></div>
+
+
     }
   </Row>
   <Modal show={show} onHide={handleClose}>
